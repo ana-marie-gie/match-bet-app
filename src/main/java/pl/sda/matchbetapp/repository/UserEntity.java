2ba6084af865be1420bc7f13978b44d227333377
ania <anagie@icloud.com>
2021-07-01 19:31:51 +0200

    UserEntity adapted to db

--- src/main/java/pl/sda/matchbetapp/repository/UserEntity.java
+++ src/main/java/pl/sda/matchbetapp/repository/UserEntity.java
@@ -2,14 +2,20 @@ package pl.sda.matchbetapp.repository;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Getter
 @Builder
+@Entity
+@Table(name = "users")
 public class UserEntity {
-    @Setter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String login;
     private String firstName;
     private String lastName;
-    private String email;
+
 }
